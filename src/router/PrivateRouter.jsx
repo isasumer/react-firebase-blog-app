b46a9a36fbdd9475cc437e7e41b7bdec4c2b5627
Isa SUMER <sumerisa5@gmail.com>
2021-10-28 15:09:34 +0300

    privateRouter added

---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Redirect, Route } from "react-router";
+import { useAuth } from "../context/AuthContextProvider";
+
+const PrivateRouter = (props) => {
+  const { currentUser } = useAuth();
+  return currentUser ? (
+    <Route path={props.path} component={props.component} />
+  ) : (
+    <Redirect to="/login" />
+  );
+};
+
+export default PrivateRouter;
