f1a6872072ffe30b33c26294a566dff4f1b5db8e
Isa SUMER <sumerisa5@gmail.com>
2021-10-30 23:40:00 +0300

    blogcontextProvideradded

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 import AuthContextProvider from "./context/AuthContextProvider";
+import { BlogContextProvider } from "./context/BlogContextProvider";
 import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
     <div className="App">
       <AuthContextProvider>
-        <AppRouter />
+        <BlogContextProvider>
+          <AppRouter />
+          <ToastContainer />
+        </BlogContextProvider>
       </AuthContextProvider>
     </div>
   );
